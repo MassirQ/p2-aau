f6b09e59f3aa95c25b5560466918de207418bb61
KamyaPA <peteraksel@live.dk>
2022-04-07 13:23:33 +0200

    code is somewhat fixed

--- server/index.js
+++ server/index.js
@@ -33,9 +33,9 @@ app.post('/start', (req,res)=>{
     "INTERNAL_DATABASE_PORT=7474\n" +
     "INTERNAL_DATABASE_PORT_2=7687\n" + 
     "BACKEND_PORT=4000\n";
-  fs.promises.readFile('./schema.graphql').
+  fs.promises.readFile('./schema.graphql',"utf8").
     then((contents) => {
-      data += contents;
+      data += "TYPEDEF=" + contents.replaceAll('\n', ' ');
       fs.promises.writeFile("./.env", data)
       .then(() => { 
           execSync("docker-compose up -d");
