0337ecb665ff6e0c07edc4879a34c3ec58e69dc1
MassirQ <aqauomi@gmail.com>
2022-04-29 00:40:41 +0200

    now it is posible to add api

--- testBackend/src/index.js
+++ testBackend/src/index.js
@@ -1,3 +1,4 @@
+const axios = require('axios').default;
 const { Neo4jGraphQL } = require("@neo4j/graphql");
 const { loadSchema } = require('@graphql-tools/load')
 const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader')
@@ -13,7 +14,7 @@ const PASSWORD = 'guc6IQaY6L1RVDbFaZea2n4k0R48rtZ_NbNjS1PnuWA';
 (async () => {
 // Create Neo4j driver instance
 // const driver = neo4j.driver(AURA_ENDPOINT, neo4j.auth.basic(USERNAME, PASSWORD), { } );
-const driver = neo4j.driver('neo4j+s://db');
+const driver = neo4j.driver('neo4j://localhost:7687',neo4j.auth.basic("neo4j", "abc123"));
 let argv = process.argv.slice(1);
 // let type = "Plain"npm;
 // let graphql = ` type ${type} {
@@ -32,13 +33,57 @@ let argv = process.argv.slice(1);
 // await fs.writeFileSync("src/schema.graphql", graphql);
 
 // let schemaDef = await fs.readFileSync("src/schema.graphql");
-let typeDefs = gql`${process.env.TYPEDEF}`;
-console.log(typeDefs)
+const resolvers ={
+  Query: {
+    getUser: async (_,{username}) => {
+      try {
+        const res =  await axios.get(`https://api.github.com/users/${username}`);
+        return res.data;
+      } catch (error) {
+        console.log(error)
+      }
+    }, 
+    getAirlines: async () => {
+      try {
+        const res = await axios.get('https://api.instantwebtools.net/v1/airlines');
+        return res.data;
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
+}
+const typeDefs = gql`
+   type Query {
+     getUser(username:String):User
+     getAirlines: [Airline]
+   }
+   type Airline {
+    id: ID,
+        name: String,
+        country: String,
+        logo: String,
+        slogan: String,
+        head_quaters: String,
+        website: String,
+        established: Int
+   } 
+   type User {
+     login: String
+     id: ID!
+     url: String
+   } 
+   type Computer {
+       make: String
+       model: String
+   }`
+// let typeDefs = gql`${process.env.TYPEDEF}`;
 
 // Create instance that contains executable GraphQL schema from GraphQL type definitions
 const neo4jGraphQL = new Neo4jGraphQL({
   typeDefs,
-  driver
+  driver,
+  resolvers
 });
 
 // Generate schema
