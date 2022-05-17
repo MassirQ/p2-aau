d6e1b2a16fa453292bc3a03710e09bc4a5094942
MassirQ <aqauomi@gmail.com>
2022-05-18 01:09:39 +0200

    Add mustasche for template generating

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var Mustache = require('mustache');
 const {execFileSync, execSync} = require('child_process')
 const app = express()
 var cors = require('cors')
@@ -8,22 +9,75 @@ var jsonParser = bodyParser.json()
 const fs = require("fs");
 
 app.use(cors());
+app.post('/createEndpointResolvers', jsonParser, (res1, req1)=>{
+  const res2 = fs.readFileSync("./tmp","utf8");
+  let lines = res2.toString().trim().split(/\n/);
+  let wrapped = "[" + lines.join(",") + "]";
+  let endpoints = JSON.parse(wrapped)
+  console.log(typeof endpoints)
+  const template = `const resolvers = {
+    Query: {
+      {{#endpointData}}
+      {{methodName}}: async () => {
+        try {
+          const res =  await axios.{{fieldMethodName}}("{{URLName}}");
+          return res.data;
+        } catch (error) {
+          console.log(error)
+        }
+      }, 
+      {{/endpointData}}
+
+        
+  }}`
+let output = Mustache.render(template, {"endpointData":endpoints});
+console.log(output)
+console.log("writing into existing file");
+fs.writeFileSync('./backend.js', output);
+})
+
+
+
 app.post('/', jsonParser,(req, res) => {
   res.send("hey")
   console.log("BODY ", req.body);
-  let fields = "";
+  let queries = [];
   for (const field of req.body.formValue) {
-    fields += `${field.fieldName}:${field.dataType} \n`
-   
+    queries.push(
+    `${field.methodName}: async () => {
+      try {
+        const res =  await axios.${field.fieldMethodName}('${field.URLName}');
+        return res.data;
+      } catch (error) {
+        console.log(error)
+      }
+    }`
+    )
   }
-  const graphql = `type ${req.body.type} {
-    ${fields}
-  }`    
+  
+  let resolverFunctions = `const resolverFunctions ={${queries.map(q => q)},}\n`
+  // const graphql = `type ${req.body.type} {
+ //   ${fields}
+ // }`    
+
+  
+for (const data of req.body.endpointData) {
+  fs.appendFileSync('./tmp', JSON.stringify(data)+ "\n" )
+}
 
 
-console.log("writing into existing file");
+//console.log("writing into existing file");
+//console.log("writing into existing file", req.body.URLName);
 
-fs.appendFileSync('./schema.graphql', graphql);
+
+//fs.appendFileSync('./schema.graphql', graphql);
+// if(req.body.step === 1) {
+   //fs.writeFileSync('./backend.js', output);
+//   fs.writeFileSync('./backend.js', customResolvers);
+// }else {
+//   fs.appendFileSync('./backend.js', )
+// } 
+
   // take information and create graphql schema
   // execFileSync("./startServer.sh", ["7474", "7687", "4000", graphql])
  
